86575864f8bba6d292ffffb6d08ce16d333e5ec3
Mac-Coleman <williamcormaccoleman@gmail.com>
2023-08-07 22:15:07 -0500

    Added animation behavior, removed unused imports, cleaned up CSS

--- main.py
+++ main.py
@@ -1,15 +1,11 @@
 from textual.app import App, ComposeResult
 from textual.widget import Widget
 from textual.widgets import Input, Static
-from textual.containers import Center, Middle
-from textual.timer import Timer
-from textual.events import Timer as TimerEvent
-from textual import on
 
 class AsciiImage(Static):
     """A class that loads a string of characters that can appear like an image."""
 
-    def __init__(self, image_file: str, fg_color: str, bg_color: str):
+    def __init__(self, image_file: str, fg_color: str, bg_color=None):
         super().__init__()
         ascii_text = open(image_file, "r").readlines()
         width = max([len(line) for line in ascii_text])
@@ -18,30 +14,32 @@ class AsciiImage(Static):
         self.fg_color = fg_color
         self.bg_color = bg_color
         self.styles.max_width = width
+        self.styles.min_width = width
         self.styles.max_height = len(ascii_text)
+        self.styles.min_height = len(ascii_text)
 
     def compose(self) -> ComposeResult:
         image = Static(self.content)
         image.styles.color = self.fg_color
-        image.styles.background = self.bg_color
+        if self.bg_color:
+            image.styles.background = self.bg_color
         yield image
 
 class C4Logo(Static):
 
     def compose(self) -> ComposeResult:
-        yield AsciiImage("assets/logo_small/less_than.txt", "white", "rgba(0,0,0,0)") #RGBA, 0 alpha
-        yield AsciiImage("assets/logo_small/c4.txt", "purple", "rgba(0,0,0,0)")
-        yield AsciiImage("assets/logo_small/greater_than.txt", "white", "rgba(0,0,0,0)")
+        yield AsciiImage("assets/logo_small/less_than.txt", "white") #RGBA, 0 alpha
+        yield AsciiImage("assets/logo_small/c4.txt", "#BB66FF")
+        yield AsciiImage("assets/logo_small/greater_than.txt", "white")
 
 class PageWrapper(Static):
 
     def compose(self) -> ComposeResult:
         yield C4Logo()
-        yield Static("Enter your name and email address to stay up to date with Cornell College Computer Science happenings!")
+        yield Static("Enter your [b][u]name[/u][/b] and [b][u]email address[/b][/u] to stay up to date with Cornell College Computer Science happenings!", classes="Prompt")
         self.name_input = Input(placeholder="Name", id="name")
         self.email_input = Input(placeholder="Email address (@cornellcollege.edu)", id="email")
-        self.thanks = Static("Placeholder text", id="thankyou")
-        self.timer = Timer(self, 5.0, repeat=1)
+        self.thanks = Static("", classes="Status")
 
         yield self.name_input
         yield self.email_input
@@ -63,11 +61,11 @@ class PageWrapper(Static):
             self.email_input.value = ""
             self.name_input.focus()
 
-        self.timer.reset()
-        self.thanks.update(f"Thanks for signing up, {first_name}!")
-        self.timer.resume()
+        self.thanks.styles.opacity = "100%"
+        self.set_timer(5.0, self.clear_text)
+        self.thanks.update(f"Thanks for signing up, [b][u][#BB66FF]{first_name}[/#BB66FF][/b][/u]!")
+        self.thanks.styles.animate("opacity", value=0.0, duration=1.0, delay=4.0)
         
-    @on(TimerEvent)
     def clear_text(self):
         self.thanks.update("")
 
@@ -78,9 +76,7 @@ class TablingApp(App):
     CSS_PATH = "main.css"        
 
     def compose(self) -> ComposeResult:
-        with Middle(id="MainContainer"):
-            with Center():
-                yield PageWrapper()
+        yield PageWrapper()
 
 if __name__ == "__main__":
     TablingApp().run()
