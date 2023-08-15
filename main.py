9fa53bccafdc5db4aae42f979c68eff39fc1229c
Mac-Coleman <williamcormaccoleman@gmail.com>
2023-08-14 22:25:37 -0500

    Add argparse and supported arguments

--- main.py
+++ main.py
@@ -3,11 +3,15 @@ from textual.binding import Binding
 from textual.widget import Widget
 from textual.widgets import Input, Static, ContentSwitcher
 from textual.containers import Center
-import random
+
 from ascii_image import AsciiImage
 from logo import C4Logo
 from signup_menu import SignupMenu
 from qr_code_menu import QrCodeMenu
+
+import random
+import argparse
+import sys
 
 class TablingApp(App):
     """Displays the tabling app."""
@@ -41,4 +45,17 @@ class TablingApp(App):
 
 
 if __name__ == "__main__":
+    parser = argparse.ArgumentParser(prog="C4 Tabling App", description="Displays a text user interface for tabling sign-up")
+    parser.add_argument("-s", "--signup-only", action="store_const", help="only display sign up window", const=True, default=False)
+    parser.add_argument("-n", "--no-backgrounds", action="store_const", help="use only a static background", const=True, default=False)
+    parser.add_argument("-q", "--qr-code", action="store", metavar="DATA", help="encode and write DATA for display as a QR-code and exit")
+    parser.add_argument("-b", "--backgrounds", action="extend", metavar="BACKGROUNDS", help="use BACKGROUNDS as backgrounds for the tabling app")
+    parser.add_argument("-v", "--version", action="version", version="%(prog)s v0.0.1")
+
+    argument_namespace = parser.parse_args()
+    
+    if argument_namespace.qr_code is not None:
+        # Encode the data as a QR code and write it to assets/qr_code/qr_code.txt
+        sys.exit(0)
+    
     TablingApp().run()
