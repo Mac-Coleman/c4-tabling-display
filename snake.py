21c4cd868a651edf6d3c73c0f0acc468bc60b742
Mac-Coleman <williamcormaccoleman@gmail.com>
2023-08-19 00:34:25 -0500

    Working on layout of snake game menu

--- snake.py
+++ snake.py
@@ -1,6 +1,9 @@
 from textual.app import ComposeResult
 from textual.widget import Widget
 from textual.widgets import Static, Label
+from textual.containers import Middle, Center
+
+from logo import C4Logo
 
 class SnakeCell(Widget):
     
@@ -8,10 +11,21 @@ class SnakeCell(Widget):
         return "HI"
 
 class SnakeGame(Static):
+
     def compose(self) -> ComposeResult:
         for i in range(15 * 15):
             yield SnakeCell()
+    
+    def on_mount(self) -> None:
+        self.border_title = "[i]Snake[/i]"
 
 class SnakeMenu(Static):
     def compose(self) -> ComposeResult:
         yield SnakeGame()
+        with Middle(classes="FitShort"):
+            with Center():
+                yield C4Logo()
+            with Center():
+                yield Label("Ready...")
+                yield Label("Set...")
+                yield Label("Go!")
