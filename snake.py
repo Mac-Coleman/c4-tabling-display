fb7f1352d340d5bb3550aca1dd07aa4255236338
Mac-Coleman <williamcormaccoleman@gmail.com>
2023-08-19 00:14:24 -0500

    Prepared Snake game grid

--- snake.py
+++ snake.py
@@ -1,9 +1,16 @@
 from textual.app import ComposeResult
+from textual.widget import Widget
 from textual.widgets import Static, Label
+
+class SnakeCell(Widget):
+    
+    def render(self):
+        return "HI"
 
 class SnakeGame(Static):
     def compose(self) -> ComposeResult:
-        yield Label("Hello!")
+        for i in range(15 * 15):
+            yield SnakeCell()
 
 class SnakeMenu(Static):
     def compose(self) -> ComposeResult:
